f0235976cfe9f4c1b652547bd66450c4b2387dcb
Mazen Siraj <94243195+MazenSiraj@users.noreply.github.com>
2023-12-14 15:48:14 +0300

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Cloud")
+option_b = os.getenv('OPTION_B', "On-prem")
 redis_password = os.getenv('REDIS_PASSWORD', "Dogs")
 hostname = socket.gethostname()
 
